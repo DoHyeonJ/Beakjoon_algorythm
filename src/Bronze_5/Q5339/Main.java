6d107d19b01a558af5d5b2ae02cfb907f9a080cb
dohyeonj <jdh7693@naver.com>
2022-10-28 17:24:17 +0900

    answer

---
+++ src/Bronze_5/Q5339/Main.java
@@ -0,0 +1,16 @@
+package Bronze_5.Q5339;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
