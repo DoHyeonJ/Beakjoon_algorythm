1a174aa907ad4d8a08273c34817b38a427cb615f
dohyeonj <jdh7693@naver.com>
2022-11-02 23:35:44 +0900

    answer

---
+++ src/Bronze_5/Q10757/Main.java
@@ -0,0 +1,15 @@
+package Bronze_5.Q10757;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.print(A.add(B));
+    }
+}
