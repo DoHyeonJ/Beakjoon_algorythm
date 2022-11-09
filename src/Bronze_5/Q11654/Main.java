07d3cda4091396600f590802c6f292dc2ddc176a
dohyeonj <jdh7693@naver.com>
2022-11-09 23:39:22 +0900

    answer

---
+++ src/Bronze_5/Q11654/Main.java
@@ -0,0 +1,13 @@
+package Bronze_5.Q11654;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char chr = sc.next().charAt(0);
+
+        System.out.print((int)chr);
+    }
+}
